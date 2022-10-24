a58a7876684eb7bb1cb9efa4528fee1827af2d79
Thatoon <Tunes.750@outlook.com>
2022-10-24 17:13:27 -0300

    COMMITADO!

--- GDF/src/view/DadosFuncionario.java
+++ GDF/src/view/DadosFuncionario.java
@@ -53,6 +53,7 @@ public class DadosFuncionario extends javax.swing.JFrame {
         jLabel11 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
+        jbSair2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -111,6 +112,18 @@ public class DadosFuncionario extends javax.swing.JFrame {
         jLabel13.setForeground(new java.awt.Color(255, 255, 255));
         jLabel13.setText("De sempre:");
 
+        jbSair2.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
+        jbSair2.setForeground(new java.awt.Color(255, 255, 255));
+        jbSair2.setText("X");
+        jbSair2.setBorder(null);
+        jbSair2.setContentAreaFilled(false);
+        jbSair2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jbSair2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbSair2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -132,11 +145,17 @@ public class DadosFuncionario extends javax.swing.JFrame {
                     .addComponent(jLabel4)
                     .addComponent(jLabel1))
                 .addContainerGap(219, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jbSair2)
+                .addGap(14, 14, 14))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(37, 37, 37)
+                .addGap(13, 13, 13)
+                .addComponent(jbSair2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel2)
@@ -179,6 +198,10 @@ public class DadosFuncionario extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbSair2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbSair2ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jbSair2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -230,5 +253,6 @@ public class DadosFuncionario extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton jbSair2;
     // End of variables declaration//GEN-END:variables
 }
